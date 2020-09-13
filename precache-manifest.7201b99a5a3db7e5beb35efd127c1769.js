b1612c326a2fed7c24f52e2c3acf6fd9ea6e6c4e
IAm-ToughCookie <toughcookie.dev@gmail.com>
2020-09-13 16:59:10 +0200

    Updates

---
+++ precache-manifest.7201b99a5a3db7e5beb35efd127c1769.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f807356bc1a2b96fa453f8eb4eef18ea",
+    "url": "/catfriends-app/index.html"
+  },
+  {
+    "revision": "f04bb0b67e3c734605b4",
+    "url": "/catfriends-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b8360f41915f3dfa476f",
+    "url": "/catfriends-app/static/css/main.03dc5a33.chunk.css"
+  },
+  {
+    "revision": "f04bb0b67e3c734605b4",
+    "url": "/catfriends-app/static/js/2.b8d52fe2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/catfriends-app/static/js/2.b8d52fe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8360f41915f3dfa476f",
+    "url": "/catfriends-app/static/js/main.8d9cbc7d.chunk.js"
+  },
+  {
+    "revision": "7d79387519c73a969549",
+    "url": "/catfriends-app/static/js/runtime-main.4f96c4bf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/catfriends-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
